8943e8839dac62a8cd658ef9b0a3736f49d0df5a
ltnsace <lijianyu741@gmail.com>
2022-06-06 21:09:42 +0800

    feat:add indicatif

--- main/src/main.rs
+++ main/src/main.rs
@@ -2,23 +2,28 @@ use core::blockchain;
 use std::thread;
 use std::time::Duration;
 
+use indicatif::ProgressBar;
+
 fn main() {
     let mut bc = blockchain::BlockChain::new_blockchain();
 
-    println!("start mining .... ");
-    thread::sleep(Duration::from_secs(5));
     bc.add_block(String::from("a -> b: 5 btc"));
     println!("produce a block !");
 
-    println!("");
-    println!("start mining .... ");
-    thread::sleep(Duration::from_secs(5));
     bc.add_block("c -> d: 1 btc".to_string());
     println!("produce a block !");
 
     for b in bc.blocks {
+
         println!("++++++++++++++++++++++++++++++++++++++++++++");
         println!("{:#?}", b);
         println!("");
+        println!("start mining .... ");
+        let pb = ProgressBar::new(1024);
+        for _ in 0..1024 {
+            pb.inc(1);
+            thread::sleep(Duration::from_millis(5));
+        }
+        pb.finish_with_message("done");
     }
 }
